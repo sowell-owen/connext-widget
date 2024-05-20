47b14291a66eaa4e763ad32091d48d935592a5e8
sowell-owen <sowell.dev@gmail.com>
2024-05-20 15:25:59 -0400

    feat: xlayer chain

--- packages/frontend/utils/toSentenceCase.ts
+++ packages/frontend/utils/toSentenceCase.ts
@@ -1,5 +1,6 @@
 // converts network name to Sentence Case (if should be in that case)
 export default function toSentenceCase(str: string) {
+    if (str === "xlayer") return "XLayer"
     if (str === "bsc") return "BSC"
     return str[0].toUpperCase() + str.slice(1, str.length);
 }
